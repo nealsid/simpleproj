daf9a8225e34b202e1fc633dcde92f303dc828c7
Neal Sidhwaney <nealsid@users.noreply.github.com>
2023-06-29 18:34:57 -0400

    Update comments/typos

--- simpleproj-sqlite.el
+++ simpleproj-sqlite.el
@@ -6,16 +6,17 @@
     ;;   (error "Sqlite database for project \"%s\" already exists" (simple-project-project-name sproj)))
 
     (let* ((db (sqlite-open sproj-sqlite-db-filename))
-           (compile-commands-json-path (concat (simple-project-build-root sproj) "/compile_commands-1-entry.json"))
-           (table-create-dml "CREATE TABLE compilation_commands (file_name text not null primary key, compile_command textworking_directory text)"))
+           (compile-commands-json-path (concat (simple-project-build-root sproj) "/compile_commands.json"))
+           (table-create-dml "CREATE TABLE compilation_commands (file_name text not null primary key, compile_command text, working_directory text)"))
       (sqlite-execute db table-create-dml)
       (sqlite-close db))))
 
 (defun parse-json-into-sqlite-table (sproj)
   ;; Sqlite does not expose READFILE through the API, only through the
-  ;; command shell, and loading the JSON is much simpler this way, so
-  ;; for this particular step, we shell out to the command line
-  ;; shell.
+  ;; command shell (or extensions, which are not present in the Emacs
+  ;; built-in SQLITE api wrapper), and loading the JSON is much
+  ;; simpler this way, so for this particular step, we shell out to
+  ;; the command line shell.
   (let ((sproj-sqlite-db-filename (concat (simple-project-build-root sproj) "/sproj-compilation-commands.sqlite3"))
         (json-load-sql (format "insert into compilation_commands (file_name, compile_command, working_directory)\
                                 select json_extract(value, '$.file'),
