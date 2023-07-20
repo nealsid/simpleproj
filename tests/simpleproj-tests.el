dd716a91a945cf7aff65fcb3648f4b7e4f191310
Neal Sidhwaney <nealsid@users.noreply.github.com>
2023-07-20 01:06:35 -0400

    Factor out common code in simpleproj-test
    - Add function create-test-compile-command-json which creates a sample
    compile_commands.json file in the requested directory
    
    - Add macro with-project-and-directory which runs a specified body
    with bindings for a project directory and project list.  I looked into
    defining a macro that wraps ert-deftest (eg, simpleproj-deftest), but
    ert-deftest unfortunately destructures arguments such as docstring &
    keyword parameters itself, which meant I would have to duplicate that
    destructuring to wrap the body, and then recombine it to pass the
    parameters to ert-deftest, which was too dependent on internal ERT
    details.

--- tests/simpleproj-tests.el
+++ tests/simpleproj-tests.el
@@ -8,6 +8,7 @@
 (require 'cl)
 
 (defun create-test-compile-command-json (project-dir)
+  "Creates a compile_commands.json file in the directory specified"
    (let* ((json-array (vector (make-hash-table)))
           (cmd-hash (aref json-array 0)))
      (puthashm "command" "gcc main.c"
@@ -17,23 +18,27 @@
          (find-file-noselect (concat project-dir "compile_commands.json") t t nil)
        (erase-buffer)
        (insert (json-serialize json-array))
-       (save-buffer)))
+       (save-buffer)
+       (kill-buffer)))
    (delete-file (concat project-dir "sproj-compilation-commands.sqlite3")))
 
 (defmacro with-project-and-directory (project-dir-name-binding &rest forms)
+  "Macro to wrap a test case body that creates bindings for the
+unique project directory for the test case as well as a
+SimpleProj project that refers to the directory."
   `(let* ((,project-dir-name-binding (concat (make-temp-file "simpleproj-" t) "/"))
           (simpleproj-projects (list (make-simple-project
-                                     :project-name "Hello, World"
-                                     :project-short-name "helloworld"
-                                     :source-root ,project-dir-name-binding
-                                     :build-root ,project-dir-name-binding))))
+                                      :project-name "Hello, World"
+                                      :project-short-name "helloworld"
+                                      :source-root ,project-dir-name-binding
+                                      :build-root ,project-dir-name-binding))))
      (progn
        (create-test-compile-command-json ,project-dir-name-binding)
        ,@forms)))
 
 (defmacro with-current-buffer-close (buffer-or-name &rest body)
-  `(with-current-buffer buffer-or-name body)
-  `(kill-buffer buffer-or-name))
+  `(with-current-buffer ,buffer-or-name body)
+  `(kill-buffer ,buffer-or-name))
 
 (defmacro sproj-deftest (name () &body docstring-keys-and-body)
   (when (stringp (car docstring-keys-and-body))
@@ -48,36 +53,9 @@
 
 (ert-deftest open-one-file-project ()
   "Opens a project with one file"
-  (with-temp-project-directory
-   project-dir
-  ;; generate json file
-   (let* ((json-array (vector (make-hash-table)))
-          (cmd-hash (aref json-array 0)))
-     (puthashm "command" "gcc main.c"
-               "directory" project-dir
-               "file" (concat project-dir "main.c") cmd-hash)
-     (with-current-buffer
-         (find-file-noselect (concat project-dir "compile_commands.json"))
-       (erase-buffer)
-       (insert (json-serialize json-array))
-       (save-buffer)))
-   (delete-file (concat project-dir "sproj-compilation-commands.sqlite3"))
-   (let ((simpleproj-projects (list (make-simple-project
-                                     :project-name "Hello, World"
-                                     :project-short-name "helloworld"
-                                     :source-root project-dir
-                                     :build-root project-dir))))
-     (with-current-buffer
-         (find-file-noselect (concat project-dir "main.c"))
-       (should (eq simpleproj-minor-mode t))
-       (should (eq simpleproj-project (nth 0 simpleproj-projects)))
-       (should (eq flymake-mode t))))))
-
-(ert-deftest open-one-file-project-2 ()
-  "Opens a project with one file"
   (with-project-and-directory
    project-dir
-   (with-current-buffer
+   (with-current-buffer-close
        (find-file-noselect (concat project-dir "main.c"))
      (should (eq simpleproj-minor-mode t))
      (should (eq simpleproj-project (nth 0 simpleproj-projects)))
@@ -87,47 +65,26 @@
   "Opens a project with one file that does not have a command line in the database"
   ;; TODO dedup code with above test.
   ;; generate json file
-  (with-temp-project-directory
+  (with-project-and-directory
    project-dir
    (message "project dir: %s" project-dir)
-   (let* ((json-array (vector (make-hash-table)))
-          (cmd-hash (aref json-array 0)))
-     (puthashm "command" "gcc main.c"
-               "directory" default-directory
-               "file" (expand-file-name (concat project-dir "main.c")) cmd-hash)
-     (with-current-buffer
-         (find-file-noselect (concat project-dir "compile_commands.json"))
-       (erase-buffer)
-       (insert (json-serialize json-array))
-       (save-buffer)))
-   (delete-file (concat project-dir "sproj-compilation-commands.sqlite3"))
-   (let ((simpleproj-projects (list (make-simple-project
-                                     :project-name "Hello, World"
-                                     :project-short-name "helloworld"
-                                     :source-root project-dir
-                                     :build-root project-dir))))
-     (with-current-buffer
-         (find-file-noselect (concat project-dir "kldjflkjd.c"))
-       (should (eq simpleproj-minor-mode t))
-       (should (eq simpleproj-project (nth 0 simpleproj-projects)))
-       (should (eq flymake-mode nil))))))
+   (with-current-buffer-close
+       (find-file-noselect (concat project-dir "kldjflkjd.c"))
+     (should (eq simpleproj-minor-mode t))
+     (should (eq simpleproj-project (nth 0 simpleproj-projects)))
+     (should (eq flymake-mode nil)))))
 
 (ert-deftest open-one-file-multiple-containing-projects ()
   "Opens a file that is contained in multiple projects in order to ensure an error is raised."
-  (with-temp-project-directory
+  (with-project-and-directory
    project-dir
    (message "project dir: %s" project-dir)
-   (let ((simpleproj-projects (list (make-simple-project
-                                     :project-name "Hello, World"
-                                     :project-short-name "helloworld"
-                                     :source-root project-dir
-                                     :build-root project-dir)
-                                    (make-simple-project
-                                     :project-name "Hello, World #2"
-                                     :project-short-name "helloworldno2"
-                                     :source-root project-dir
-                                     :build-root project-dir))))
-     (with-current-buffer
-         (find-file-noselect (concat project-dir "kldjflkjd.c"))
-       (should (eq simpleproj-minor-mode nil))
-       (should (eq simpleproj-project nil))))))
+   (push (make-simple-project
+          :project-name "Hello, World #2"
+          :project-short-name "helloworldno2"
+          :source-root project-dir
+          :build-root project-dir) simpleproj-projects)
+   (with-current-buffer-close
+       (find-file-noselect (concat project-dir "kldjflkjd.c"))
+     (should (eq simpleproj-minor-mode nil))
+     (should (eq simpleproj-project nil)))))
