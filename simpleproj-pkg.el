6837b275fd8a36daf04e73342478300d7c3622f8
Neal Sidhwaney <nealsid@users.noreply.github.com>
2023-08-06 16:52:20 -0400

    Some functions to help with packaging

--- simpleproj-pkg.el
+++ simpleproj-pkg.el
@@ -1,2 +1,31 @@
-(define-package "SimpleProj" ".9" "SimpleProj is an elisp library to help manage project-related development tasks."
-  nil)
+(define-package "simpleproj" "0.7" "SimpleProj is an elisp library to help manage project-related development tasks." nil)
+
+(setq sproj--proj-name "simpleproj")
+(setq sproj--version "0.7")
+(setq sproj--root-dir "/home/nealsid/git/simpleprojelisp/")
+
+(setq sproj--file-list
+      '("simpleproj-pkg.el"
+        "core/simpleproj.el"
+        "core/simpleproj-db-init-and-load.el"
+        "core/simpleproj-db-query.el"
+        "core/util.el"
+        "features/simpleproj-cc-json-functions.el"
+        "features/simpleproj-flymake.el"
+        "tests/main.c"
+        "tests/run-tests.sh"
+        "tests/simpleproj-tests.el"
+        "tests/util-test.el"))
+
+(defun sproj--make-elisp-pkg ()
+  (let* ((packaging-root-dir (concat (make-temp-file "simpleproj-packaging-" t) "/"))
+         (package-relative-directory (concat sproj--proj-name "-" sproj--version "/"))
+         (package-root-dir (concat packaging-root-dir package-relative-directory))
+         (package-tar-fn (concat sproj--proj-name "-" sproj--version ".tar"))
+         (default-directory packaging-root-dir))
+    (make-directory package-root-dir)
+    (mapc (lambda (fn)
+            (copy-file (concat sproj--root-dir fn) (concat package-root-dir (file-name-nondirectory fn))))
+          sproj--file-list)
+    (shell-command (concat "tar cvf " package-tar-fn " " package-relative-directory))
+    (dired default-directory)))
